797d6aa3315d68d70a72d7fef024684ca89bbee9
Marcin Pająk <mpajak@wsei.edu.pl>
2021-10-16 15:21:47 +0200

    Added Book MVC

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibApp_Gr3.Models;
+
+namespace LibApp_Gr3.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET/Books/Random
+        public IActionResult Random()
+        {
+            var firstBook = new Book() { Name = "English dictionary" };
+
+            return View(firstBook);
+        }
+    }
+}
