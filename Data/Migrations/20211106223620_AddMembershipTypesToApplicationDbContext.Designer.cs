544d4d35b75e311c10e5c3a0b414bf7a5a5c7226
pajakwsei <mpajak@wsei.edu.pl>
2021-11-06 23:36:56 +0100

    Added MembershipTypes to database

--- Data/Migrations/20211106223620_AddMembershipTypesToApplicationDbContext.Designer.cs
+++ Data/Migrations/20211106223620_AddMembershipTypesToApplicationDbContext.Designer.cs
@@ -4,14 +4,16 @@ using LibApp_Gr3.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LibApp_Gr3.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211106223620_AddMembershipTypesToApplicationDbContext")]
+    partial class AddMembershipTypesToApplicationDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
